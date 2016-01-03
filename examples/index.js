6f1fa00b1a5cda3fff551318aa6ba64d5008d6e4
spikef <spikef@foxmail.com>
2016-01-03 23:46:13 +0800

    rewrite in pure js

--- examples/index.js
+++ examples/index.js
@@ -1,59 +1,3 @@
-# react-native-gesture-password
-
-A gesture password component for React Native. It supports both iOS and Android since it's' written in pure JavaScript.
-
-一个React Native的手势密码组件，纯JavaScript实现，因此同时支持iOS和安卓平台。
-
-![image](https://github.com/Spikef/react-native-gesture-password/raw/master/screenshot.gif)
-
-## Install
-
-```javascript
-npm install react-native-gesture-password --save
-```
-
-## Properties
-
-All properties bellow are optional.
-
-### message (string)
-
-The message text you want to show.
-
-### status (string)
-
-Can be 'normal', 'right' or 'wrong'.
-
-The gesture password don't validate your password. You should do that yourself, and tell the result by status.
-
-### rightColor (string)
-
-Use this color to render when status !== 'wrong'.
-
-### wrongColor (string)
-
-Use this color to render when status === 'wrong'.
-
-### interval (number)
-
-The active circles will be reset automatically after you give an interval.
-
-### onStart (function)
-
-Event raised when user touch a number circle.
-
-### onEnd (function)
-
-Event raised when user finish input a password.
-
-### children
-
-Other components that you want to display.
-
-## Examples
-
-```javascript
-
 var React = require('react-native');
 var {
     AppRegistry,
@@ -147,10 +91,3 @@ var AppDemo = React.createClass({
 });
 
 AppRegistry.registerComponent('AppDemo', () => AppDemo);
-
-```
-## Notes
-
-This old version(<0.1.0) is at the branch native. I won't update that unless fix bugs.
-
-If you have suggestions or bug reports, feel free to send pull request or [create new issue](https://github.com/spikef/react-native-gesture-password/issues/new).
--- index.js
+++ index.js
@@ -1,73 +1,257 @@
-'use strict';
-
 var React = require('react-native');
-var { requireNativeComponent } = React;
+var {
+    StyleSheet,
+    PropTypes,
+    Dimensions,
+    PanResponder,
+    View,
+    Text
+    } = React;
 
-class GesturePassword extends React.Component {
-    constructor() {
-        super();
-        this._onEnd = this._onEnd.bind(this);
-        this._onStart = this._onStart.bind(this);
-        this._onCancel = this._onCancel.bind(this);
+var Width = Dimensions.get('window').width;
+var Height = Dimensions.get('window').height;
+var Top = (Height - Width)/2.0 * 1.5;
+var Radius = Width / 10;
+var Diameter = Radius * 2;
+
+var Circle = React.createClass({
+    propTypes: {
+        color: PropTypes.string,
+        fill: PropTypes.bool,
+        x: PropTypes.number,
+        y: PropTypes.number
+    },
+    render: function() {
+        return (
+            <View style={[circles.outer,
+                        {left: this.props.x - Radius, top: this.props.y - Radius},
+                        this.props.fill && {borderColor: this.props.color}]}>
+                {this.props.fill && <View style={[circles.inner, {backgroundColor: this.props.color}]} />}
+            </View>
+        )
     }
-    reset(text) {
-        this.refs.password.reset(text);
+});
+
+var GesturePassword = React.createClass({
+    timer: null,
+    lastIndex: -1,
+    sequence: '',   // 手势结果
+    propTypes: {
+        message: PropTypes.string,
+        rightColor: PropTypes.string,
+        wrongColor: PropTypes.string,
+        status: PropTypes.oneOf(['right', 'wrong', 'normal']),
+        onStart: PropTypes.func,
+        onEnd: PropTypes.func,
+        interval: PropTypes.number
+    },
+    getDefaultProps: function() {
+        return {
+            message: '',
+            rightColor: '#5FA8FC',
+            wrongColor: '#D93609',
+            status: 'normal',
+            interval: 0
+        }
+    },
+    getInitialState: function() {
+        var circles = [];
+        var Margin = Radius;
+        for (let i=0; i < 9; i++) {
+            let p = i % 3;
+            let q = parseInt(i / 3);
+            circles.push({
+                isActive: false,
+                x: p * (Radius * 2 + Margin) + Margin + Radius,
+                y: q * (Radius * 2 + Margin) + Margin + Radius
+            });
+        }
+
+        return {
+            circles: circles,
+            Lines: []
+        }
+    },
+    componentWillMount: function() {
+        this._panResponder = PanResponder.create({
+            // 要求成为响应者：
+            onStartShouldSetPanResponder: (event, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (event, gestureState) => true,
+            onMoveShouldSetPanResponder: (event, gestureState) => true,
+            onMoveShouldSetPanResponderCapture: (event, gestureState) => true,
+
+            // 开始手势操作
+            onPanResponderGrant: (event, gestureState) => {
+                this.onStart(event, gestureState);
+            },
+            // 移动操作
+            onPanResponderMove: (event, gestureState) => {
+                this.onMove(event, gestureState);
+            },
+            // 释放手势
+            onPanResponderRelease: (event, gestureState) => {
+                this.onEnd(event, gestureState);
+            }
+        });
+    },
+    render: function() {
+        var textColor = this.props.status === 'wrong' ? this.props.wrongColor : this.props.rightColor;
+
+        return (
+            <View style={[styles.frame, this.props.style, {flex: 1}]}>
+                <View style={styles.message}>
+                    <Text style={[styles.msgText, {color: textColor}]}>
+                        {this.state.message || this.props.message}
+                    </Text>
+                </View>
+                <View style={styles.board} {...this._panResponder.panHandlers}>
+                        {this.renderCircles()}
+                </View>
+                {this.state.Lines}
+                {this.props.children}
+            </View>
+        )
+    },
+    renderCircles: function() {
+        var array = [], fill, color;
+        var{ status, wrongColor, rightColor } = this.props;
+
+        this.state.circles.forEach(function(c, i) {
+            fill = c.isActive;
+            color = status === 'wrong' ? wrongColor : rightColor;
+
+            array.push(
+                <Circle key={i} fill={fill} color={color} x={c.x} y={c.y} />
+            )
+        });
+
+        return array;
+    },
+    setActive: function(index) {
+        this.state.circles[index].isActive = true;
+        this.forceUpdate();
+    },
+    resetActive: function() {
+        for (let i=0; i < 9; i++) {
+            this.state.circles[i].isActive = false;
+        }
+
+        this.forceUpdate();
+    },
+    getTouchChar: function(touch) {
+        var x = touch.x;
+        var y = touch.y;
+
+        for (let i=0; i < 9; i++) {
+            if ( helper.isPointInCircle({x, y}, this.state.circles[i], Radius) ) {
+                return String(i);
+            }
+        }
+
+        return false;
+    },
+    onStart: function(e, g) {
+        var x = e.nativeEvent.pageX;
+        var y = e.nativeEvent.pageY - Top;
+
+        var lastChar = this.getTouchChar({x, y});
+        if ( lastChar ) {
+            this.lastIndex = Number(lastChar);
+            this.sequence = lastChar;
+            this.resetActive();
+            this.setActive(this.lastIndex);
+
+            this.props.onStart && this.props.onStart();
+
+            if ( this.props.interval>0 ) {
+                clearTimeout(this.timer);
+            }
+        }
+    },
+    onMove: function(e, g) {
+        var x = e.nativeEvent.pageX;
+        var y = e.nativeEvent.pageY - Top;
+
+        if ( this.lastIndex>-1 && !helper.isPointInCircle({x, y}, this.state.circles[this.lastIndex], Radius) ) {
+            var lastChar = this.getTouchChar({x, y});
+            if ( lastChar && this.sequence.indexOf(lastChar) === -1 ) {
+                this.lastIndex = Number(lastChar);
+                this.sequence += lastChar;
+                this.setActive(this.lastIndex);
+            }
+        }
+    },
+    onEnd: function(e, g) {
+        if ( this.lastIndex>-1 ) {
+            var password = helper.getRealPassword(this.sequence);
+            this.sequence = '';
+            this.lastIndex = -1;
+
+            this.props.onEnd && this.props.onEnd(password);
+
+            if ( this.props.interval>0 ) {
+                this.timer = setTimeout(() => this.resetActive(), this.props.interval);
+            }
+        }
     }
-    setNativeProps(props) {
-        this.refs.password.setNativeProps(props);
+});
+
+var helper = {
+    isPointInCircle: function(point, center, radius) {
+        var a = Math.pow((center.x - point.x), 2);
+        var b = Math.pow((center.y - point.y), 2);
+        var d = Math.sqrt(a + b);
+
+        return d <= radius;
+    },
+    getRealPassword: function(str) {
+        return str.replace(/\d/g, function($0) {
+            return Number($0) + 1;
+        });
     }
-    _onStart() {
-        this.props.onStart && this.props.onStart();
-    }
-    _onCancel() {
-        this.props.onCancel && this.props.onCancel();
-    }
-    _onEnd(event: Event) {
-        var e = event.nativeEvent;
-        this.props.onEnd && this.props.onEnd(e.password);
-    }
-    render() {
-        var nativeProps = Object.assign({}, this.props);
-        nativeProps.onStart = this._onStart;
-        nativeProps.onCancel = this._onCancel;
-        nativeProps.onEnd = this._onEnd;
-
-        return <RNGesturePassword ref='password' {...nativeProps} />;
-    }
-}
-
-GesturePassword.propTypes = {
-    /**
-     * 默认提示文本
-     */
-    inputDefault: React.PropTypes.string,
-
-    /**
-     * 当前提示文本
-     */
-    inputTipText: React.PropTypes.string,
-
-    /**
-     * 密码是否输入有误
-     */
-    gestureError: React.PropTypes.bool,
-
-    /**
-     * 开始输入
-     */
-    onStart: React.PropTypes.func,
-
-    /**
-     * 结束输入
-     */
-    onEnd: React.PropTypes.func,
-
-    /**
-     * 取消输入
-     */
-    onCancel: React.PropTypes.func
 };
 
-var RNGesturePassword = requireNativeComponent('RNGesturePassword', GesturePassword);
+var styles = StyleSheet.create({
+    frame: {
+        backgroundColor: '#292B38'
+    },
+    board: {
+        position: 'absolute',
+        left: 0,
+        top: Top,
+        width: Width,
+        height: Height
+    },
+    message: {
+        position: 'absolute',
+        left: 0,
+        top: Top / 2.2,
+        width: Width,
+        height: Top / 3,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    msgText: {
+        fontSize: 14
+    }
+});
+
+var circles = StyleSheet.create({
+    outer: {
+        position: 'absolute',
+        width: Diameter,
+        height: Diameter,
+        borderRadius: Radius,
+        borderColor: '#8E91A8',
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inner: {
+        width: Diameter / 3,
+        height: Diameter / 3,
+        borderRadius: Radius / 3
+    }
+});
 
 module.exports = GesturePassword;
