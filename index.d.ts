6294b1207c29f5cd889838a71892b3f9e63a8b33
hossein mohammadi <hosseinm.developer@gmail.com>
2019-12-11 15:10:52 +0330

    Typescript declaration

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,12 @@
 import * as React from 'react';
+import { ViewStyle, TextStyle } from 'react-native';
 
 export type GesPasswordStates = 'normal' | 'right' | 'wrong'
 
 export interface GesturePasswordProps {
+    style?: ViewStyle,
+    transparentLine?: boolean,
+    textStyle?: TextStyle,
     message?: string,
     normalColor?: string,
     rightColor?: string,
@@ -17,5 +21,5 @@ export interface GesturePasswordProps {
     onReset?: () => void
 }
 
-declare class GesturePassword extends React.Component<GesturePasswordProps> {}
+declare class GesturePassword extends React.Component<GesturePasswordProps> { }
 export default GesturePassword;
